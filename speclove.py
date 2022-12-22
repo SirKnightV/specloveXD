ee970ed0f5332be94ef0b1ff1ccbe4ea5fb6b56a
BioHazzard <71390611+SirKnightV@users.noreply.github.com>
2022-12-22 11:36:15 -0600

    Add files via upload

---
+++ speclove.py
@@ -0,0 +1,8 @@
+import random
+lista = 'ohh si dame dame mas me gusta que riko 7u7'
+negrosmaduros = lista
+pap = negrosmaduros
+while True:
+    a = 0
+    for a in iter(lambda: a+1, -1):
+        print(f"A Spec se la estan metiendo {a} negros y dice que {pap}")
